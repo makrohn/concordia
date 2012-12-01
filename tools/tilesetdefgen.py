e11592471ce6aa15e9a40a888ef08f950fa42b91
Matthew Krohn <makrohn@gmail.com>
2012-12-01 16:02:13 -0500

    Added copyright headers to python tools

--- tools/tilesetdefgen.py
+++ tools/tilesetdefgen.py
@@ -1,3 +1,18 @@
+#Copyright © 2011-2012 Matthew Krohn
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 print "This script assumes all tiles in your tileset block are the same height, will be centered horizontally, offset up by half a tile, and the tile block is left justified."
 tile = int(raw_input("Starting tile number: "))
 gridx = 32
