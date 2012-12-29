003ecdd99621d1ac7b6a19023a48c887e34a965f
Matthew Krohn <makrohn@gmail.com>
2012-12-29 08:05:10 -0500

    Updated tilesetdefgen.py script to be more flexible and intuitive

--- tools/tilesetdefgen.py
+++ tools/tilesetdefgen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
 #Copyright © 2011-2012 Matthew Krohn
 #
 #This program is free software: you can redistribute it and/or modify
@@ -14,25 +16,36 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 print "This script assumes all tiles in your tileset block are the same height, will be centered horizontally, offset up by half a tile, and the tile block is left justified."
-tile = int(raw_input("Starting tile number: "))
-gridx = 32
-gridy = 32
-tilex = 32
+tile = 16
+tilesetname = raw_input("Tilesetdef: ")
+tilesetimage = raw_input("Tileset image: ")
+imagex = int(raw_input("Width of the tileset image: "))
+gridsize = raw_input("Game Grid Tile Size (x*y): ")
+gridsize = gridsize.split("*")
+gridx = int(gridsize[0])
+gridy = int(gridsize[1])
+tilex = int(gridsize[0])
 tiley = int(raw_input("Height of a tile in this tileset block: "))
-imagex = 960
-y = int(raw_input("Top edge of the tile block: "))
-imagey = int(raw_input("Bottom edge of the tile block: "))
-tilesetname = raw_input("Tileset: ")
-filepath = tilesetname
-x=0
-tileline=""
+y = 0
+imagey = int(raw_input("Height of the tile block: "))
+x = 0
 imagex = imagex - (imagex % gridx)
-while y < imagey:
-    while x < imagex:
-        tileline = "".join(("tile=",str(tile),",",str(x),",",str(y),",",str(tilex),",",str(tiley),",",str(tilex-(gridx/2)),",",str(tiley-(gridy/2)),"\n"))
-        with open(filepath, "a") as tileset:
-            tileset.write(tileline)
-        x += tilex
-        tile +=1
-    y += tiley
-    x = 0
+done = False
+with open(tilesetname, "w") as tileset:
+	tileset.write("img=" + tilesetimage + "\n" + "\n")
+	while done == False:
+		while y < imagey:
+		    while x < imagex:
+		        tileset.write("tile=" + str(tile) + "," + str(x) + "," + str(y) + "," + str(tilex) + "," + str(tiley) + "," + str(tilex-(gridx/2)) + "," + str(tiley-(gridy/2)) + "\n")
+		        x += tilex
+		        tile +=1
+		    y += tiley
+		    x = 0
+		tileset.write("\n")
+		areyoudone = raw_input("Are there more tiles in this tileset? [y/n] ")
+		if areyoudone == "y":
+			done = False
+			tiley = int(raw_input("Height of a tile in this tileset block: "))
+			imagey = imagey + int(raw_input("Height of the tile block: "))
+		if areyoudone == "n":
+			done = True
