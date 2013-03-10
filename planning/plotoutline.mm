b040330502df63fb8bafe5c145bcd0ac44bb0993
Matthew Krohn <makrohn@gmail.com>
2013-03-09 19:05:59 -0500

    Uploading some planning files for Concordia

---
+++ planning/plotoutline.mm
@@ -0,0 +1,92 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1362866422594" ID="ID_1281095052" MODIFIED="1362871931441" TEXT="Concordia">
+<node CREATED="1362871467833" HGAP="90" ID="ID_882304823" MODIFIED="1362872250492" POSITION="left" STYLE="bubble" TEXT="Labor Camp" VSHIFT="-71">
+<node CREATED="1362870967895" HGAP="-46" ID="ID_1789632152" MODIFIED="1362873117677" STYLE="fork" TEXT="Undead Fortress" VSHIFT="200">
+<attribute NAME="Dungeon" VALUE="3"/>
+<attribute NAME="Level" VALUE="4"/>
+<attribute NAME="Weapon" VALUE="2"/>
+</node>
+<node CREATED="1362866436708" HGAP="156" ID="ID_204536838" MODIFIED="1362872197297" STYLE="fork" TEXT="Maroon" VSHIFT="-62">
+<arrowlink DESTINATION="ID_71118472" ENDARROW="Default" ENDINCLINATION="5;-41;" ID="Arrow_ID_624056921" STARTARROW="None" STARTINCLINATION="196;0;"/>
+<attribute NAME="Dungeon" VALUE="1"/>
+<attribute NAME="Town" VALUE="1"/>
+</node>
+<node CREATED="1362871792908" HGAP="15" ID="ID_1843641920" MODIFIED="1362871890633" STYLE="fork" TEXT="Mines" VSHIFT="-413">
+<attribute NAME="Dungeon" VALUE="1"/>
+<node CREATED="1362866494981" HGAP="50" ID="ID_71118472" MODIFIED="1362873181348" STYLE="fork" TEXT="Caves" VSHIFT="-8">
+<cloud/>
+<attribute NAME="Dungeon" VALUE="2"/>
+<attribute NAME="Level" VALUE="2"/>
+<node CREATED="1362870536402" HGAP="80" ID="ID_1138626532" MODIFIED="1362873314512" TEXT="Underground Town" VSHIFT="-60">
+<attribute NAME="Town" VALUE="2"/>
+<attribute NAME="Armor" VALUE="1"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1362871936859" ID="ID_1721128066" MODIFIED="1362872401175" POSITION="right" TEXT="Port">
+<attribute NAME="Town" VALUE="3"/>
+<attribute NAME="Level" VALUE="5"/>
+<attribute NAME="Weapon" VALUE="3"/>
+<attribute NAME="Armor" VALUE="2"/>
+<node CREATED="1362872268129" ID="ID_321708575" MODIFIED="1362873127798" STYLE="bubble" TEXT="World Map">
+<attribute NAME="Level" VALUE="5"/>
+<node CREATED="1362872456573" HGAP="84" ID="ID_1248066768" MODIFIED="1362873254493" STYLE="fork" TEXT="Mountains" VSHIFT="-54">
+<arrowlink DESTINATION="ID_514964145" ENDARROW="Default" ENDINCLINATION="92;0;" ID="Arrow_ID_1061792483" STARTARROW="None" STARTINCLINATION="92;0;"/>
+<attribute NAME="Dungeon" VALUE="4"/>
+<attribute NAME="Level" VALUE="6"/>
+</node>
+<node CREATED="1362872300568" HGAP="63" ID="ID_1746183458" MODIFIED="1362873254492" STYLE="fork" TEXT="Midtown" VSHIFT="43">
+<arrowlink DESTINATION="ID_1248066768" ENDARROW="Default" ENDINCLINATION="17;57;" ID="Arrow_ID_381045009" STARTARROW="None" STARTINCLINATION="-12;-72;"/>
+<attribute NAME="Town" VALUE="4"/>
+<attribute NAME="Level" VALUE="6"/>
+<attribute NAME="Armor" VALUE="3"/>
+<attribute NAME="Weapon" VALUE="3"/>
+</node>
+<node CREATED="1362872530800" HGAP="73" ID="ID_514964145" MODIFIED="1362873144011" STYLE="fork" TEXT="Tower Outpost" VSHIFT="62">
+<attribute NAME="Dungeon" VALUE="5"/>
+<attribute NAME="Level" VALUE="8"/>
+<node CREATED="1362872597141" HGAP="136" ID="ID_1095926657" MODIFIED="1362873149518" STYLE="bubble" TEXT="World Map" VSHIFT="27">
+<attribute NAME="Level" VALUE="9"/>
+<node CREATED="1362872641162" ID="ID_1069771553" MODIFIED="1362873200766" STYLE="fork" TEXT="Orc Enclave">
+<cloud/>
+<attribute NAME="Level" VALUE="10"/>
+<attribute NAME="Town" VALUE="5"/>
+<attribute NAME="Armor" VALUE="4"/>
+<attribute NAME="Weapon" VALUE="4"/>
+<node CREATED="1362872705007" HGAP="28" ID="ID_435172278" MODIFIED="1362873222183" TEXT="Desert Palace" VSHIFT="-34">
+<arrowlink DESTINATION="ID_960058268" ENDARROW="Default" ENDINCLINATION="14;-21;" ID="Arrow_ID_1762694661" STARTARROW="None" STARTINCLINATION="32;31;"/>
+<attribute NAME="Level" VALUE="11"/>
+<attribute NAME="Armor" VALUE="5"/>
+<attribute NAME="Dungeon" VALUE="6"/>
+</node>
+<node CREATED="1362872781257" ID="ID_960058268" MODIFIED="1362873222184" TEXT="Ice Palace">
+<arrowlink DESTINATION="ID_328677238" ENDARROW="Default" ENDINCLINATION="62;-47;" ID="Arrow_ID_271967660" STARTARROW="None" STARTINCLINATION="-71;82;"/>
+<attribute NAME="Level" VALUE="12"/>
+<attribute NAME="Armor" VALUE="5"/>
+<attribute NAME="Dungeon" VALUE="7"/>
+</node>
+</node>
+<node CREATED="1362872859251" HGAP="12" ID="ID_328677238" MODIFIED="1362873232278" TEXT="Skeleton Homeland" VSHIFT="59">
+<cloud/>
+<attribute NAME="Level" VALUE="13"/>
+<node CREATED="1362872913026" HGAP="68" ID="ID_298028248" MODIFIED="1362873164475" STYLE="fork" TEXT="Human Resistance" VSHIFT="32">
+<attribute NAME="Level" VALUE="14"/>
+<attribute NAME="Town" VALUE="6"/>
+<attribute NAME="Armor" VALUE="5"/>
+<attribute NAME="Weapon" VALUE="5"/>
+<node CREATED="1362873010751" HGAP="73" ID="ID_1423068499" MODIFIED="1362873164476" TEXT="Imperial Palace" VSHIFT="-60">
+<attribute NAME="Level" VALUE="15"/>
+<attribute NAME="Dungeon" VALUE="8"/>
+<attribute NAME="Armor" VALUE="6"/>
+<attribute NAME="Weapon" VALUE="6"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
